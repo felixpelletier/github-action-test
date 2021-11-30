f9663d3421d2ea5edfafc8c26bd333f4dbd365cc
Félix Pelletier <felixpelletier23@gmail.com>
2021-11-29 23:32:30 -0500

    Percentage rendering

--- .github/generate_net56_progress_image.py
+++ .github/generate_net56_progress_image.py
@@ -7,17 +7,30 @@ import click
 @click.option('--output', default="net56_progress.png")
 def generate_image(width, height, output):
 
-    net5_percentage = 0.1
+    total_crawler_count = 33
+    net5_crawler_count = 17
+    net5_ratio = float(net5_crawler_count) / float(total_crawler_count)
+
+    LINE_W = 2.0
+    INNER_MARGIN = 4.0 + LINE_W
+    TOP_MARGIN = 0.3 * height
 
     surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, width, height)
     ctx = cairo.Context(surface)
-    ctx.scale(width, height)  # Normalizing the canvas
 
-    ctx.rectangle(0, 0, net5_percentage * 0.9, 1)
-    ctx.set_source_rgb(0.18, 0.80, 0.25)
+    ctx.rectangle(LINE_W / 2, LINE_W / 2 + TOP_MARGIN, 1.0 * (width - LINE_W), 1 * (height - LINE_W - TOP_MARGIN))
+    ctx.set_source_rgb(0.00, 0.00, 0.00)
+    ctx.set_line_width(LINE_W)
+    ctx.set_line_cap(cairo.LINE_CAP_BUTT)
+    ctx.stroke()
+
+    ctx.rectangle(INNER_MARGIN / 2, INNER_MARGIN / 2 + TOP_MARGIN, net5_ratio * (width - INNER_MARGIN), 1 * (height - INNER_MARGIN - TOP_MARGIN))
+    ctx.set_source_rgb(0.18, 0.80, 0.25) # Some kind of green.
     ctx.fill()
 
-    ctx.move_to(0, 0)
+    ctx.move_to(LINE_W, TOP_MARGIN * 0.90)
+    ctx.set_font_size(TOP_MARGIN * 0.95)
+    ctx.show_text(f"{net5_ratio * 100:.0f}% ({net5_crawler_count}/{total_crawler_count})")
 
     surface.write_to_png(output)
 
